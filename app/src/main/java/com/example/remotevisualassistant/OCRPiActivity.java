61e7aee5db32a89a65caab124317889a18258b5e
SaiEsvarVipparthy <saiesvar@gmail.com>
2019-04-17 04:11:40 +0530

    Changes and Correction to- capture image from stream

--- app/src/main/java/com/example/remotevisualassistant/OCRPiActivity.java
+++ app/src/main/java/com/example/remotevisualassistant/OCRPiActivity.java
@@ -1,6 +1,10 @@
 package com.example.remotevisualassistant;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Picture;
+import android.graphics.drawable.PictureDrawable;
 import android.media.Image;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
@@ -11,7 +15,12 @@ import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.util.HashMap;
 
 public class OCRPiActivity extends AppCompatActivity {
@@ -32,7 +41,52 @@ public class OCRPiActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
 //                imageView.setImageBitmap(getVideoFrameFromVideo(vid_url));
-                imageView.setImageURI(Uri.parse(vid_url));
+                webView.stopLoading();;
+//                webView.setDrawingCacheEnabled(true);
+//                Bitmap bitmap = Bitmap.createBitmap(webView.getDrawingCache());
+//                webView.setDrawingCacheEnabled(false);
+
+//                Picture p = webView.capturePicture();
+////                PictureDrawable pd = new PictureDrawable(p);
+//                Bitmap bitmap = Bitmap.createBitmap(p.getWidth()
+//                        ,p.getHeight()
+//                        , Bitmap.Config.ARGB_8888);
+//                Canvas canvas = new Canvas(bitmap);
+//                p.draw(canvas);
+//                imageView.draw(canvas);
+
+                //to save as an image on device
+                Picture picture = webView.capturePicture();
+                Bitmap  b = Bitmap.createBitmap( picture.getWidth(),
+                        picture.getHeight(), Bitmap.Config.ARGB_8888);
+                Canvas c = new Canvas( b );
+
+                picture.draw( c );
+                FileOutputStream fos = null;
+                try {
+
+                    fos = new FileOutputStream( "mnt/sdcard/capture.jpg" );
+                    if ( fos != null )
+                    {
+                        b.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                        Toast.makeText(OCRPiActivity.this,"image saved",Toast.LENGTH_SHORT).show();
+                        fos.close();
+                        try {
+                            File f=new File("mnt/sdcard/", "capture.jpg");
+                            Bitmap b_in = BitmapFactory.decodeStream(new FileInputStream(f));
+                            imageView.setImageBitmap(b_in);
+                        }
+                        catch (FileNotFoundException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                catch( Exception e )
+                {
+                    Toast.makeText(OCRPiActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                }
+                webView.reload();
             }
         });
 
