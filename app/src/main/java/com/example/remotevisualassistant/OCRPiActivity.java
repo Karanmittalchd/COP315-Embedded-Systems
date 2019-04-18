9d26b6e482a9b67444c9fec633dc0aaed317293d
SaiEsvarVipparthy <saiesvar@gmail.com>
2019-04-18 15:59:45 +0530

    Changes and Correction to- OCRPiActivity- included ocr text by karan. but hard coded to http://10.194.105.20.8081

--- app/src/main/java/com/example/remotevisualassistant/OCRPiActivity.java
+++ app/src/main/java/com/example/remotevisualassistant/OCRPiActivity.java
@@ -1,5 +1,6 @@
 package com.example.remotevisualassistant;
 
+import android.content.DialogInterface;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
@@ -9,13 +10,20 @@ import android.media.Image;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
 import android.os.Build;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.SparseArray;
 import android.view.View;
 import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.google.android.gms.vision.Frame;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -25,9 +33,11 @@ import java.util.HashMap;
 
 public class OCRPiActivity extends AppCompatActivity {
 
-    private Button b_capture, b_stream;
+    private Button b_capture, b_stream, b_ocr;
     private ImageView imageView;
     private WebView webView;
+    private TextView tresult;
+    boolean playing;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,58 +45,97 @@ public class OCRPiActivity extends AppCompatActivity {
         setContentView(R.layout.activity_ocrpi);
 
         set_UI_components();
-        final String vid_url = "http://10.194.105.20:8081/";
+
+        b_ocr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(playing){
+                    playing=false;
+                    tresult.bringToFront();
+                    webView.stopLoading();;
+
+                    Picture picture = webView.capturePicture();
+                    Bitmap  b = Bitmap.createBitmap( picture.getWidth(),
+                            picture.getHeight(), Bitmap.Config.ARGB_8888);
+
+                    webView.reload();
+
+                    TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
+                    if(!textRecognizer.isOperational())
+                    {
+                        playing = true;
+                        Toast.makeText(getApplicationContext(),"Could not get the Text",Toast.LENGTH_SHORT).show();
+                    }
+                    else{
+                        Frame frame = new Frame.Builder().setBitmap(b).build();
+                        SparseArray<TextBlock> items = textRecognizer.detect(frame);
+                        StringBuilder sb = new StringBuilder();
+                        for(int i=0; i<items.size(); ++i){
+                            TextBlock myItem = items.valueAt(i);
+                            sb.append(myItem.getValue());
+                            sb.append("\n");
+
+                        }
+                        imageView.setVisibility(View.INVISIBLE);
+                        tresult.setVisibility(View.VISIBLE);
+                        tresult.bringToFront();
+                        tresult.setText("OCR Result: "+sb.toString());
+                    }
+
+                    playing=true;
+                }
+                else{
+                    build_an_alert("Capture Failed","Must frist start stream to do ocr","okay");
+                }
+            }
+        });
 
         b_capture.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-//                imageView.setImageBitmap(getVideoFrameFromVideo(vid_url));
-                webView.stopLoading();;
-//                webView.setDrawingCacheEnabled(true);
-//                Bitmap bitmap = Bitmap.createBitmap(webView.getDrawingCache());
-//                webView.setDrawingCacheEnabled(false);
+                if(playing){
+                    playing=false;
+                    webView.stopLoading();;
 
-//                Picture p = webView.capturePicture();
-////                PictureDrawable pd = new PictureDrawable(p);
-//                Bitmap bitmap = Bitmap.createBitmap(p.getWidth()
-//                        ,p.getHeight()
-//                        , Bitmap.Config.ARGB_8888);
-//                Canvas canvas = new Canvas(bitmap);
-//                p.draw(canvas);
-//                imageView.draw(canvas);
+                    Picture picture = webView.capturePicture();
+                    Bitmap  b = Bitmap.createBitmap( picture.getWidth(),
+                            picture.getHeight(), Bitmap.Config.ARGB_8888);
+                    Canvas c = new Canvas( b );
 
-                //to save as an image on device
-                Picture picture = webView.capturePicture();
-                Bitmap  b = Bitmap.createBitmap( picture.getWidth(),
-                        picture.getHeight(), Bitmap.Config.ARGB_8888);
-                Canvas c = new Canvas( b );
-
-                picture.draw( c );
-                FileOutputStream fos = null;
-                try {
-
-                    fos = new FileOutputStream( "mnt/sdcard/capture.jpg" );
-                    if ( fos != null )
-                    {
-                        b.compress(Bitmap.CompressFormat.JPEG, 100, fos);
-                        Toast.makeText(OCRPiActivity.this,"image saved",Toast.LENGTH_SHORT).show();
-                        fos.close();
-                        try {
-                            File f=new File("mnt/sdcard/", "capture.jpg");
-                            Bitmap b_in = BitmapFactory.decodeStream(new FileInputStream(f));
-                            imageView.setImageBitmap(b_in);
-                        }
-                        catch (FileNotFoundException e)
+                    picture.draw( c );
+                    FileOutputStream fos = null;
+                    try {
+                        fos = new FileOutputStream( "mnt/sdcard/capture.jpg" );
+                        if ( fos != null )
                         {
-                            e.printStackTrace();
+                            b.compress(Bitmap.CompressFormat.JPEG, 100, fos);
+                            Toast.makeText(OCRPiActivity.this,"image saved",Toast.LENGTH_SHORT).show();
+                            fos.close();
+                            try {
+                                File f=new File("mnt/sdcard/", "capture.jpg");
+                                Bitmap b_in = BitmapFactory.decodeStream(new FileInputStream(f));
+                                imageView.setVisibility(View.VISIBLE);
+                                tresult.setVisibility(View.INVISIBLE);
+                                imageView.bringToFront();
+                                imageView.setImageBitmap(b_in);
+                            }
+                            catch (FileNotFoundException e)
+                            {
+                                e.printStackTrace();
+                            }
                         }
                     }
+                    catch( Exception e )
+                    {
+                        Toast.makeText(OCRPiActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                    }
+                    webView.reload();
+                    playing=true;
                 }
-                catch( Exception e )
-                {
-                    Toast.makeText(OCRPiActivity.this,e.getMessage(),Toast.LENGTH_SHORT).show();
+                else{
+                    build_an_alert("Capture Failed","Must frist start stream to capture","okay");
                 }
-                webView.reload();
+
             }
         });
 
@@ -94,12 +143,12 @@ public class OCRPiActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String vid_url = "http://10.194.105.20:8081";
-//                webView.scrollTo(webView.get,webView);
                 webView.getSettings().setBuiltInZoomControls(true);
                 webView.getSettings().setDisplayZoomControls(false);
                 webView.getSettings().setLoadWithOverviewMode(true);
                 webView.getSettings().setUseWideViewPort(true);
                 webView.loadUrl(vid_url);
+                playing=true;
             }
         });
     }
@@ -109,5 +158,29 @@ public class OCRPiActivity extends AppCompatActivity {
         b_stream = (Button)findViewById(R.id.button_stream);
         imageView = (ImageView)findViewById(R.id.imageViewCapture);
         webView = (WebView)findViewById(R.id.webView2);
+        b_ocr = (Button)findViewById(R.id.button_ocr);
+        tresult = (TextView)findViewById(R.id.t_ocrresult);
+
+        imageView.setVisibility(View.INVISIBLE);
+        tresult.setVisibility(View.INVISIBLE);
+    }
+
+    private void build_an_alert(String t, String m, String b){
+        android.support.v7.app.AlertDialog.Builder builder = new AlertDialog.Builder(OCRPiActivity.this);
+        builder.setTitle(t);
+        builder.setMessage(m);
+        builder.setCancelable(false);
+        builder.setPositiveButton(
+                b,
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.cancel();
+
+                    }
+                }
+        );
+        AlertDialog alert1 = builder.create();
+        alert1.show();
     }
 }
